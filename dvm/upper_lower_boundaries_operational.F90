fc2bac074fd1d543c6c4b0667946a0a2fd66f75e
caglartac <vc.yumruktepe@gmail.com>
2026-03-19 14:41:41 +0100

    upper lower operational

---
+++ dvm/upper_lower_boundaries_operational.F90
@@ -0,0 +1,259 @@
+#include "fabm_driver.h"
+
+module dvm_upper_lower_boundaries_operational
+
+use fabm_types
+use fabm_expressions
+
+implicit none
+
+private 
+
+type, extends(type_base_model), public :: type_upper_lower_boundaries_operational
+
+    type (type_dependency_id)                       :: id_par, id_migrator_food, id_depth 
+    type (type_surface_dependency_id)               :: id_par0
+    type (type_diagnostic_variable_id)              :: id_present
+    type (type_bottom_dependency_id)                :: id_topo
+    type (type_horizontal_dependency_id)            :: id_daylength
+    type (type_state_variable_id)                   :: id_oxygen
+
+    real(rk) :: night_threshold
+    real(rk) :: oxygen_threshold
+    real(rk) :: upper_boundary
+    real(rk) :: lower_boundary
+    real(rk) :: lower_boundary_night
+    real(rk) :: depth_threshold
+
+    contains
+        procedure :: initialize
+        procedure :: do
+
+end type
+
+contains
+
+    subroutine initialize(self, configunit)
+        class (type_upper_lower_boundaries_operational), intent(inout), target :: self
+        integer, intent(in)                                  :: configunit
+        call self%register_diagnostic_variable(self%id_present,'migrator_presence','-','migrators are present here')
+
+        call self%register_dependency(self%id_par, standard_variables%downwelling_photosynthetic_radiative_flux)
+        call self%register_dependency(self%id_par0, standard_variables%surface_downwelling_photosynthetic_radiative_flux)
+        call self%register_dependency(self%id_migrator_food,'migrator_food','mgC/m3','food availability for the migrators')
+        !call self%register_dependency(self%id_migrator_food0,vertical_integral(self%id_migrator_food))
+!        call self%register_dependency(self%id_depth,standard_variables%pressure)
+!        call self%register_dependency(self%id_topo,standard_variables%bottom_depth )
+        call self%register_dependency(self%id_depth,standard_variables%depth)
+        call self%register_dependency(self%id_topo,standard_variables%bottom_depth_below_geoid )
+        call self%register_dependency(self%id_daylength,'daylength','hours','number of hours light is available at the surface')
+        call self%register_state_dependency(self%id_oxygen, 'oxygen', 'mmol m-3', 'oxygen concentration')
+
+        call self%get_parameter(self%oxygen_threshold,'oxygen_threshold','mmol m-3','anoxic conditions where below migrator will not swim into',default=62.53_rk)
+        call self%get_parameter(self%night_threshold,'night_threshold','log10(W/m2)','light level where below is night to active swimming to surface',default=0.0_rk)
+        call self%get_parameter(self%upper_boundary,'upper_boundary','log10(W/m2)','light level of the upper boundary for high migrator concentration',default=-6.5_rk)
+        call self%get_parameter(self%lower_boundary,'lower_boundary','log10(W/m2)','light level of the daytime lower boundary for high migrator concentration',default=-15.0_rk)
+        call self%get_parameter(self%lower_boundary_night,'lower_boundary_night','log10(W/m2)','light level of the nighttime lower boundary for high migrator concentration',default=-15.0_rk)
+        call self%get_parameter(self%depth_threshold,'depth_threshold','m','depth where below migrator will not swim into',default=500.0_rk)
+    end subroutine initialize
+
+    subroutine do(self, _ARGUMENTS_DO_)
+
+        class (type_upper_lower_boundaries_operational), intent(in) :: self
+        _DECLARE_ARGUMENTS_DO_
+    
+        real(rk) :: par, par0, nhours, food
+        real(rk) :: parlog, par0log
+        real(rk) :: depth
+        real(rk) :: upper_presence, lower_presence
+        real(rk) :: topo
+        real(rk) :: oxygen
+
+        _LOOP_BEGIN_
+
+            _GET_SURFACE_(self%id_par0,par0)
+            _GET_SURFACE_(self%id_daylength,nhours)
+            !_GET_SURFACE_(self%id_migrator_food0,food)
+            _GET_BOTTOM_(self%id_topo,topo)
+            _GET_(self%id_oxygen,oxygen)
+
+            par0log = max(-20.0_rk, log10(par0))
+
+            _GET_(self%id_par,par)
+            _GET_(self%id_depth,depth)
+
+            parlog = max(-20.0_rk, log10(par))
+
+            ! SPECIFY THE POSSIBLE LOCATIONS OF HIGH MIGRATOR CONCENTRATION !
+
+            ! There are 4 cases
+            ! 1. Winter Arctic night (surface parmean < 1E-10)
+            ! 2. Summer Arctic night (number of daylight hours > 23.9)
+            ! 3. Normal day cycle day time
+            ! 4. Normal day cycle night time
+
+            ! CASE 1
+            upper_presence = 0.0_rk
+            lower_presence = 0.0_rk
+
+!            if (nhours < 0.05_rk) then
+!                upper_presence = 0.0_rk
+!                lower_presence = 0.0_rk
+!                
+!                    if (depth < 240.0_rk) then
+!                        upper_presence = 1.0_rk
+!                    else
+!                        upper_presence = 0.0_rk
+!                    end if
+!                
+!                ! Set diagnostic based on presence
+!                if (upper_presence + lower_presence > 0.9_rk .and. oxygen >= self%oxygen_threshold) then
+!                    _SET_DIAGNOSTIC_(self%id_present, 1.0_rk)
+!                else
+!                    _SET_DIAGNOSTIC_(self%id_present, 0.0_rk)
+!                end if
+!                
+!            else
+!
+!                ! CASE 2
+!                if (nhours > 23.95_rk) then
+!                    ! there is an upper and a lower light boundary
+!                    ! first calculate possibilities above the lower boundary
+!
+!                    ! Initialize presence variables
+!                    upper_presence = 0.0_rk
+!                    lower_presence = 0.0_rk
+!                    
+!                    ! Lowerlight Rules
+!                            ! if (parmeanlog > -15.0_rk) then
+!                            !     upper_presence = 1.0_rk
+!                            ! else
+!                            !     upper_presence = 0.0_rk
+!                            ! end if
+!
+!
+!
+!                    ! Lower light rules
+!                    if (parmean0log <= 0.67_rk) then
+!                        if (parmeanlog > -15.84_rk) then
+!                            upper_presence = 1.0_rk
+!                        else
+!                            upper_presence = 0.0_rk
+!                        end if
+!                    else
+!                        if (parmeanlog > -11.01_rk) then
+!                            upper_presence = 1.0_rk
+!                        else
+!                            upper_presence = 0.0_rk
+!                        end if
+!                    end if
+!
+!                    ! Upperlight Rules
+!                    if (parmean0log <= 0.65_rk) then
+!                        if (parlog < -8.0_rk) then
+!                            lower_presence = 1.0_rk
+!                        else
+!                            lower_presence = 0.0_rk
+!                        end if
+!                    else
+!                        if (parlog < -4.0_rk) then
+!                            lower_presence = 1.0_rk
+!                        else
+!                            lower_presence = 0.0_rk
+!                        end if
+!                    end if
+!
+!
+!                    ! Set diagnostic based on presence
+!                    if (upper_presence + lower_presence > 1.0_rk) then
+!                        _SET_DIAGNOSTIC_(self%id_present, 1.0_rk)
+!                    else
+!                        if (upper_presence > 0.9_rk .and. depth >= max(topo - 20.0_rk, 0.0_rk) .and. oxygen >= self%oxygen_threshold) then 
+!                            _SET_DIAGNOSTIC_(self%id_present,1.0_rk)
+!                        else 
+!                            _SET_DIAGNOSTIC_(self%id_present, 0.0_rk)
+!                        end if
+!                    end if
+!                else
+
+                    ! CASE 3
+                    if (par0log > self%night_threshold) then
+                        ! there is an upper and a lower light boundary
+                        ! first calculate possibilities above the lower boundary
+                        
+                        ! Initialize presence variables
+                        upper_presence = 0.0_rk
+                        lower_presence = 0.0_rk
+                        
+                        ! Lowerlight Rules
+                        !if (parmeanlog > self%lower_boundary) then
+                        if (depth > self%depth_threshold) then
+                            upper_presence = 1.0_rk
+                        else
+                            upper_presence = 0.0_rk
+                        end if
+                        
+                        ! Upperlight Rules
+                        if (parlog < self%upper_boundary) then
+                            lower_presence = 1.0_rk
+                        else
+                            lower_presence = 0.0_rk
+                        end if
+                        
+                        
+                        ! Set diagnostic based on presence
+                        if (upper_presence + lower_presence > 1.0_rk) then
+                            _SET_DIAGNOSTIC_(self%id_present, 1.0_rk)
+                        else
+                            if (upper_presence > 0.9_rk .and. depth >= max(topo - 20.0_rk, 0.0_rk) .and. oxygen >= self%oxygen_threshold) then 
+                                _SET_DIAGNOSTIC_(self%id_present,1.0_rk)
+                            else 
+                                _SET_DIAGNOSTIC_(self%id_present, 0.0_rk)
+                            end if
+                        end if
+                        
+                    else
+                        ! CASE 4
+                        ! Initialize presence variables
+                        upper_presence = 0.0_rk
+                        lower_presence = 0.0_rk
+                        
+                        ! Calculate possibilities above the lower boundary
+                        !if (parmeanlog > self%lower_boundary_night) then
+                        if (depth > self%depth_threshold) then
+                            upper_presence = 1.0_rk
+                        else
+                            upper_presence = 0.0_rk
+                        end if
+                        
+                        ! Set diagnostic based on presence
+                        if (upper_presence + lower_presence > 0.9_rk .and. oxygen >= self%oxygen_threshold) then
+                            _SET_DIAGNOSTIC_(self%id_present, 1.0_rk)
+                        else
+                            _SET_DIAGNOSTIC_(self%id_present, 0.0_rk)
+                        end if
+                        
+                    end if
+!                end if
+
+            !end if
+
+            ! This should ensure that each point at least receives the 0.0_rk value
+            if (upper_presence + lower_presence < 0.9_rk) then
+                if (depth > self%depth_threshold) then
+                            upper_presence = 1.0_rk
+                else
+                   _SET_DIAGNOSTIC_(self%id_present,0.0_rk) 
+                end if    
+            end if 
+            ! 
+
+
+            ! ------------------------------------------------------------- !
+
+
+        _LOOP_END_
+
+    end subroutine do
+
+end module
--- nersc_model_library.F90
+++ nersc_model_library.F90
@@ -23,6 +23,7 @@ contains
       use dvm_conservative_migrator
       use dvm_get_dependencies
       use dvm_upper_lower_boundaries_simple
+      use dvm_upper_lower_boundaries_operational
       use dvm_weight_distribution
       use dvm_move
       use fabm_nersc_icealgae_G
@@ -40,6 +41,7 @@ contains
          case ('dvm_conservative_migrator'); allocate(type_conservative_migrator::model)
          case ('dvm_get_dependencies'); allocate(type_get_dependencies::model)
          case ('dvm_upper_lower_boundaries_simple'); allocate(type_upper_lower_boundaries_simple::model)
+         case ('dvm_upper_lower_boundaries_operational'); allocate(type_upper_lower_boundaries_operational::model)
          case ('dvm_weight_distribution'); allocate(type_weight_distribution::model)
          case ('dvm_move'); allocate(type_move::model)
          case ('icealgae_G'); allocate(type_nersc_icealgae_G::model)
